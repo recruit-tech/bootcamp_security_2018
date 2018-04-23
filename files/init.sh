bbb3a376c02175d89a8bcd467602ac97e3c37c62
Toshiharu Sugiyama <31683948+harupur@users.noreply.github.com>
2018-04-23 17:00:38 +0900

    mysqlコマンドの消し忘れを対応。

--- files/init.sh
+++ files/init.sh
@@ -3,7 +3,6 @@ cd /var/www/app/sns/;
 rake db:create RAILS_ENV=production
 rake db:reset RAILS_ENV=production
 chown www-data:www-data /var/www/app/sns/db/production.sqlite3
-mysql -u root -ptoor -D bad_sns_production < /root/bad_sns_production.sql
 systemctl daemon-reload
 export LC_ALL="en_US.UTF-8"
 bundle exec unicorn_rails -c config/unicorn.rb -E production -D
